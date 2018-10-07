341992611a3b807487c2f480f33d78b5be5a971a
Ajay <ajay@Srijans-MacBook-Air-2.local>
2018-10-08 00:47:58 +0530

    please aus.js in git reppo

---
+++ aus.js
@@ -0,0 +1,28 @@
+let num = Number(prompt("Pick a number"));
+
+if (num < 10) {
+  console.log("Small");
+} else if (num < 100) {
+  console.log("Medium");
+} else {
+  console.log("Large");
+}
+let result = 1;
+let counter = 0;
+while (counter < 10) {
+  result = result * 2-counter;
+  counter = counter + 1;
+}
+console.log(result);
+// → 1024
+
+let numb=String(prompt("Write you name"))
+if(numb<String.length===4)
+{
+    console.log("Welcome to Srijan")
+}
+else 
+{
+    console.log("Welcome you")
+}
+
