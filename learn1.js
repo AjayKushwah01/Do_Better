b95dff00a0650ccd0b249598e404f731e64efbc0
Ajay <ajay@Srijans-MacBook-Air-2.local>
2018-10-08 00:44:19 +0530

    add the js file

---
+++ learn1.js
@@ -0,0 +1,25 @@
+// (function () {
+//     x = sumAll(12, 56, 34, 56, 89, 47)
+//     function sumAll() {
+//         let i=0
+//         let sum = 0
+//         for (; i < arguments.length; i++) {
+//             sum = sum + arguments[i];
+//         }
+//         return sum
+//     }
+//     console.log("the max value is", x)
+// })();
+
+(function ()
+{
+    function retro(opp)
+    {
+        return opp + "it was a good experience";
+    }
+
+let value=retro("retrospective")
+console.log(value)
+
+()
+ })
